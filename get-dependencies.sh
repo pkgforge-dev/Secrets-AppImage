61bcf001b82f3dbec9fce57ea6cb3b62fc0cfb2a
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-26 14:38:21 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,9 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm secrets \
-                        libcups \
-                        libusb
+pacman -Syu --noconfirm secrets
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
